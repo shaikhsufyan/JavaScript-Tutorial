3ea59dbd48832eb64e83d0d8c05b2b6b602e54bb
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-03-27 11:55:14 +0530

    file 11 update

--- File11/File11.js
+++ File11/File11.js
@@ -6,11 +6,13 @@
  if the condition is true, then it will repeat the loop as long
  as the condition is true.*/
   
- let num = 10;
- let i = 11;
+
+ let i = 0;
 
  do{
     console.log("Number is "+ i);
     i++;
  }
  while(i<=10);
+ 
+ 
