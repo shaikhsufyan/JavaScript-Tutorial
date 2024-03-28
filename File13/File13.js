f11fb0c17b78f87cde4ffc2f23e9f110da1edd06
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-03-28 12:29:42 +0530

    update code

--- File13/File13.js
+++ File13/File13.js
@@ -1,21 +1,18 @@
 // String in Javascript ::
-// A Javascript string is zero or more characters written inside quotes.
-
-
-let firstName="CODE WITH SUFYAN KHAN";
-console.log(firstName);
+// A Javascript string is zero or more characters written inside
+//  quotes.
+  
+let str = "Hello";
+console.log( str.search("l"));
 
 // Index
-// s u f y a n
-// 0 1 2 3 4 5
+// c o d e
+// 0 1 2 3 
 
 // Find length :
 // We can find the length of string
 
-
-// Escape :
-// We can use double qoutes inside double double quote using escape
-
+ 
 
 // IndexOf :
 // we can find index number of any string
