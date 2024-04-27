ae15c8d5688c066f1bd2f08a30db6509c1671288
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-04-27 12:54:29 +0530

    Map Filter Reduce

---
+++ Map(),Filter(),Reduce()/Filter.js
@@ -0,0 +1,9 @@
+//  USe of Filter function
+
+const array=[4,6,8,3,9];
+ 
+ const output=array.filter(function greter(x){
+     
+     return x>4;
+ });
+ console.log(output);
---
+++ Map(),Filter(),Reduce()/map.js
@@ -0,0 +1,10 @@
+//  map transform the value  in three way
+// double,triple and binary
+
+ const array=[2,5,7,3,4];
+
+ const output=array.map(function double(x){
+     return x*2;
+ })
+ console.log(output);
+  
