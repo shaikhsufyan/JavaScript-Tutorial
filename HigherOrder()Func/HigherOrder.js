882b16b8c077fe0ddf5e01dff470f7ab97274c62
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-04-28 11:27:53 +0530

    hidher order function in js

---
+++ HigherOrder()Func/HigherOrder.js
@@ -0,0 +1,10 @@
+// Higher Order Function :: Function inside an another function first function use as argument in th esecond function
+
+function x(){
+    console.log("sufyan");
+}
+function y(x){
+    console.log("shaikh");
+}
+
+y(x());
