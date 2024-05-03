8e1787526d52aed9dc06597b6426eeeb241fbf01
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-05-03 12:57:14 +0530

    this keyword in js

---
+++ thisClg.js
@@ -0,0 +1,14 @@
+// object & this keyword challenge :
+
+// Make object get firstname from the object ?
+
+// What wil be the output of these program :
+// let data = {
+//     name : "sufyan",
+//     age : 20,
+//     proff :"Engineerr",
+//     check(){
+//         console.log(this.age);
+//     }
+// }
+// data.check();
