237f2c035b17e4388c7c96c31226ca3ed692b903
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-05-08 00:24:50 +0530

    function in js

---
+++ FunctionClg.js
@@ -0,0 +1,245 @@
+// Function Challenge :
+
+// Write Diff b/w function parameter & function argument
+
+// parameter:
+// function creation par variale pass kare 
+
+function check(lname){
+    console.log("Code With Sufyan "+lname);
+}
+check("shaikh")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// LET'S EXPLORBE
+
+// BB
+// HC
+// FC
+// GT A E A M
+// C
+// A
+
+
+
+// Career :
+
+// Coding Job 
+// Big StartUP 
+// FANG 
+// US 
+// Company 
+// Course like iNeuron
+// YOUTUBE 
+
+// Business : 
+
+// Real State 
+// Colony 
+// Farming's
+// child
+// investment
+// Restaurent
+// spread business
+
+// w
+
+
+
+
+
+
+
+
+
+
+
+
+
+
